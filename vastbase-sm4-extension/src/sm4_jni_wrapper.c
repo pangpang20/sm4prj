3e21ab29013dddcf6db3ede73e453314056e02fd
陈云亮 <yunliang.chen@audaque.com>
2025-12-24 15:46:26 +0800

    update code

--- vastbase-sm4-extension/src/sm4_jni_wrapper.c
+++ vastbase-sm4-extension/src/sm4_jni_wrapper.c
@@ -25,29 +25,29 @@ static void set_error(const char *msg) {
 
 /* 检查并清理JNI异常 */
 static int check_and_clear_exception(JNIEnv *env) {
-    if ((*env)->ExceptionCheck(env)) {
-        jthrowable exception = (*env)->ExceptionOccurred(env);
-        (*env)->ExceptionClear(env);
+    if (env->ExceptionCheck()) {
+        jthrowable exception = env->ExceptionOccurred();
+        env->ExceptionClear();
         
         /* 尝试获取异常消息 */
-        jclass throwable_class = (*env)->FindClass(env, "java/lang/Throwable");
+        jclass throwable_class = env->FindClass("java/lang/Throwable");
         if (throwable_class != NULL) {
-            jmethodID getMessage = (*env)->GetMethodID(env, throwable_class,
+            jmethodID getMessage = env->GetMethodID(throwable_class,
                                                         "getMessage", "()Ljava/lang/String;");
             if (getMessage != NULL) {
-                jstring msg = (jstring)(*env)->CallObjectMethod(env, exception, getMessage);
+                jstring msg = (jstring)env->CallObjectMethod(exception, getMessage);
                 if (msg != NULL) {
-                    const char *msg_str = (*env)->GetStringUTFChars(env, msg, NULL);
+                    const char *msg_str = env->GetStringUTFChars(msg, NULL);
                     set_error(msg_str);
-                    (*env)->ReleaseStringUTFChars(env, msg, msg_str);
-                    (*env)->DeleteLocalRef(env, msg);
+                    env->ReleaseStringUTFChars(msg, msg_str);
+                    env->DeleteLocalRef(msg);
                 }
             }
-            (*env)->DeleteLocalRef(env, throwable_class);
+            env->DeleteLocalRef(throwable_class);
         }
         
         if (exception != NULL) {
-            (*env)->DeleteLocalRef(env, exception);
+            env->DeleteLocalRef(exception);
         }
         
         return -1;
@@ -89,50 +89,50 @@ int sm4_jni_init_if_needed(const char *jar_path) {
     }
     
     /* 查找Java类 */
-    g_sm4_utils_class = (*env)->FindClass(env, "com/alibaba/datax/utils/SM4Utils");
+    g_sm4_utils_class = env->FindClass("com/alibaba/datax/utils/SM4Utils");
     if (g_sm4_utils_class == NULL || check_and_clear_exception(env) != 0) {
         set_error("Failed to find SM4Utils class");
         return -1;
     }
-    g_sm4_utils_class = (*env)->NewGlobalRef(env, g_sm4_utils_class);
+    g_sm4_utils_class = (jclass)env->NewGlobalRef(g_sm4_utils_class);
     
-    g_sm4_keygen_class = (*env)->FindClass(env, "com/alibaba/datax/pljava/SM4KeyGenerator");
+    g_sm4_keygen_class = env->FindClass("com/alibaba/datax/pljava/SM4KeyGenerator");
     if (g_sm4_keygen_class == NULL || check_and_clear_exception(env) != 0) {
         set_error("Failed to find SM4KeyGenerator class");
         return -1;
     }
-    g_sm4_keygen_class = (*env)->NewGlobalRef(env, g_sm4_keygen_class);
+    g_sm4_keygen_class = (jclass)env->NewGlobalRef(g_sm4_keygen_class);
     
     /* 获取方法ID */
-    g_method_encrypt_hex = (*env)->GetStaticMethodID(env, g_sm4_utils_class,
+    g_method_encrypt_hex = env->GetStaticMethodID(g_sm4_utils_class,
         "encryptGcmKeyHexResultHex", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
     if (g_method_encrypt_hex == NULL || check_and_clear_exception(env) != 0) {
         set_error("Failed to find encryptGcmKeyHexResultHex method");
         return -1;
     }
     
-    g_method_decrypt_hex = (*env)->GetStaticMethodID(env, g_sm4_utils_class,
+    g_method_decrypt_hex = env->GetStaticMethodID(g_sm4_utils_class,
         "decryptGcmKeyHexValueHex", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
     if (g_method_decrypt_hex == NULL || check_and_clear_exception(env) != 0) {
         set_error("Failed to find decryptGcmKeyHexValueHex method");
         return -1;
     }
     
-    g_method_encrypt_base64 = (*env)->GetStaticMethodID(env, g_sm4_utils_class,
+    g_method_encrypt_base64 = env->GetStaticMethodID(g_sm4_utils_class,
         "encryptKey64Result64", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
     if (g_method_encrypt_base64 == NULL || check_and_clear_exception(env) != 0) {
         set_error("Failed to find encryptKey64Result64 method");
         return -1;
     }
     
-    g_method_decrypt_base64 = (*env)->GetStaticMethodID(env, g_sm4_utils_class,
+    g_method_decrypt_base64 = env->GetStaticMethodID(g_sm4_utils_class,
         "decryptKeyBase64Value64", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
     if (g_method_decrypt_base64 == NULL || check_and_clear_exception(env) != 0) {
         set_error("Failed to find decryptKeyBase64Value64 method");
         return -1;
     }
     
-    g_method_generate_key = (*env)->GetStaticMethodID(env, g_sm4_keygen_class,
+    g_method_generate_key = env->GetStaticMethodID(g_sm4_keygen_class,
         "generateKey", "()Ljava/lang/String;");
     if (g_method_generate_key == NULL || check_and_clear_exception(env) != 0) {
         set_error("Failed to find generateKey method");
@@ -149,10 +149,10 @@ JNIEnv* sm4_jni_get_env(void) {
         return NULL;
     }
     
-    jint result = (*g_jvm)->GetEnv(g_jvm, (void**)&env, JNI_VERSION_1_8);
+    jint result = g_jvm->GetEnv((void**)&env, JNI_VERSION_1_8);
     if (result == JNI_EDETACHED) {
         /* 当前线程未附加到JVM，需要附加 */
-        result = (*g_jvm)->AttachCurrentThread(g_jvm, (void**)&env, NULL);
+        result = g_jvm->AttachCurrentThread((void**)&env, NULL);
         if (result != JNI_OK) {
             return NULL;
         }
@@ -177,18 +177,18 @@ int sm4_jni_generate_key(char *key_out, int key_size) {
         return -1;
     }
     
-    result = (jstring)(*env)->CallStaticObjectMethod(env, g_sm4_keygen_class, 
+    result = (jstring)env->CallStaticObjectMethod(g_sm4_keygen_class, 
                                                        g_method_generate_key);
     if (check_and_clear_exception(env) != 0 || result == NULL) {
         if (result == NULL) set_error("Failed to generate key");
         return -1;
     }
     
-    key_str = (*env)->GetStringUTFChars(env, result, NULL);
+    key_str = env->GetStringUTFChars(result, NULL);
     strncpy(key_out, key_str, key_size - 1);
     key_out[key_size - 1] = '\0';
-    (*env)->ReleaseStringUTFChars(env, result, key_str);
-    (*env)->DeleteLocalRef(env, result);
+    env->ReleaseStringUTFChars(result, key_str);
+    env->DeleteLocalRef(result);
     
     return 0;
 }
@@ -205,31 +205,31 @@ int sm4_jni_encrypt_hex(const char *plain_text, const char *hex_key,
         return -1;
     }
     
-    j_key = (*env)->NewStringUTF(env, hex_key);
-    j_plain = (*env)->NewStringUTF(env, plain_text);
+    j_key = env->NewStringUTF(hex_key);
+    j_plain = env->NewStringUTF(plain_text);
     
-    result = (jstring)(*env)->CallStaticObjectMethod(env, g_sm4_utils_class,
+    result = (jstring)env->CallStaticObjectMethod(g_sm4_utils_class,
                                                        g_method_encrypt_hex, j_key, j_plain);
     
-    (*env)->DeleteLocalRef(env, j_key);
-    (*env)->DeleteLocalRef(env, j_plain);
+    env->DeleteLocalRef(j_key);
+    env->DeleteLocalRef(j_plain);
     
     if (check_and_clear_exception(env) != 0 || result == NULL) {
         if (result == NULL) set_error("Encryption failed");
         return -1;
     }
     
-    cipher_str = (*env)->GetStringUTFChars(env, result, NULL);
-    if (strlen(cipher_str) >= cipher_size) {
+    cipher_str = env->GetStringUTFChars(result, NULL);
+    if (strlen(cipher_str) >= (size_t)cipher_size) {
         set_error("Cipher buffer too small");
-        (*env)->ReleaseStringUTFChars(env, result, cipher_str);
-        (*env)->DeleteLocalRef(env, result);
+        env->ReleaseStringUTFChars(result, cipher_str);
+        env->DeleteLocalRef(result);
         return -1;
     }
     
     strcpy(cipher_out, cipher_str);
-    (*env)->ReleaseStringUTFChars(env, result, cipher_str);
-    (*env)->DeleteLocalRef(env, result);
+    env->ReleaseStringUTFChars(result, cipher_str);
+    env->DeleteLocalRef(result);
     
     return 0;
 }
@@ -246,31 +246,31 @@ int sm4_jni_decrypt_hex(const char *cipher_text, const char *hex_key,
         return -1;
     }
     
-    j_key = (*env)->NewStringUTF(env, hex_key);
-    j_cipher = (*env)->NewStringUTF(env, cipher_text);
+    j_key = env->NewStringUTF(hex_key);
+    j_cipher = env->NewStringUTF(cipher_text);
     
-    result = (jstring)(*env)->CallStaticObjectMethod(env, g_sm4_utils_class,
+    result = (jstring)env->CallStaticObjectMethod(g_sm4_utils_class,
                                                        g_method_decrypt_hex, j_key, j_cipher);
     
-    (*env)->DeleteLocalRef(env, j_key);
-    (*env)->DeleteLocalRef(env, j_cipher);
+    env->DeleteLocalRef(j_key);
+    env->DeleteLocalRef(j_cipher);
     
     if (check_and_clear_exception(env) != 0 || result == NULL) {
         if (result == NULL) set_error("Decryption failed");
         return -1;
     }
     
-    plain_str = (*env)->GetStringUTFChars(env, result, NULL);
-    if (strlen(plain_str) >= plain_size) {
+    plain_str = env->GetStringUTFChars(result, NULL);
+    if (strlen(plain_str) >= (size_t)plain_size) {
         set_error("Plain buffer too small");
-        (*env)->ReleaseStringUTFChars(env, result, plain_str);
-        (*env)->DeleteLocalRef(env, result);
+        env->ReleaseStringUTFChars(result, plain_str);
+        env->DeleteLocalRef(result);
         return -1;
     }
     
     strcpy(plain_out, plain_str);
-    (*env)->ReleaseStringUTFChars(env, result, plain_str);
-    (*env)->DeleteLocalRef(env, result);
+    env->ReleaseStringUTFChars(result, plain_str);
+    env->DeleteLocalRef(result);
     
     return 0;
 }
@@ -287,31 +287,31 @@ int sm4_jni_encrypt_base64(const char *plain_text, const char *base64_key,
         return -1;
     }
     
-    j_key = (*env)->NewStringUTF(env, base64_key);
-    j_plain = (*env)->NewStringUTF(env, plain_text);
+    j_key = env->NewStringUTF(base64_key);
+    j_plain = env->NewStringUTF(plain_text);
     
-    result = (jstring)(*env)->CallStaticObjectMethod(env, g_sm4_utils_class,
+    result = (jstring)env->CallStaticObjectMethod(g_sm4_utils_class,
                                                        g_method_encrypt_base64, j_key, j_plain);
     
-    (*env)->DeleteLocalRef(env, j_key);
-    (*env)->DeleteLocalRef(env, j_plain);
+    env->DeleteLocalRef(j_key);
+    env->DeleteLocalRef(j_plain);
     
     if (check_and_clear_exception(env) != 0 || result == NULL) {
         if (result == NULL) set_error("Encryption failed");
         return -1;
     }
     
-    cipher_str = (*env)->GetStringUTFChars(env, result, NULL);
-    if (strlen(cipher_str) >= cipher_size) {
+    cipher_str = env->GetStringUTFChars(result, NULL);
+    if (strlen(cipher_str) >= (size_t)cipher_size) {
         set_error("Cipher buffer too small");
-        (*env)->ReleaseStringUTFChars(env, result, cipher_str);
-        (*env)->DeleteLocalRef(env, result);
+        env->ReleaseStringUTFChars(result, cipher_str);
+        env->DeleteLocalRef(result);
         return -1;
     }
     
     strcpy(cipher_out, cipher_str);
-    (*env)->ReleaseStringUTFChars(env, result, cipher_str);
-    (*env)->DeleteLocalRef(env, result);
+    env->ReleaseStringUTFChars(result, cipher_str);
+    env->DeleteLocalRef(result);
     
     return 0;
 }
@@ -328,31 +328,31 @@ int sm4_jni_decrypt_base64(const char *cipher_text, const char *base64_key,
         return -1;
     }
     
-    j_key = (*env)->NewStringUTF(env, base64_key);
-    j_cipher = (*env)->NewStringUTF(env, cipher_text);
+    j_key = env->NewStringUTF(base64_key);
+    j_cipher = env->NewStringUTF(cipher_text);
     
-    result = (jstring)(*env)->CallStaticObjectMethod(env, g_sm4_utils_class,
+    result = (jstring)env->CallStaticObjectMethod(g_sm4_utils_class,
                                                        g_method_decrypt_base64, j_key, j_cipher);
     
-    (*env)->DeleteLocalRef(env, j_key);
-    (*env)->DeleteLocalRef(env, j_cipher);
+    env->DeleteLocalRef(j_key);
+    env->DeleteLocalRef(j_cipher);
     
     if (check_and_clear_exception(env) != 0 || result == NULL) {
         if (result == NULL) set_error("Decryption failed");
         return -1;
     }
     
-    plain_str = (*env)->GetStringUTFChars(env, result, NULL);
-    if (strlen(plain_str) >= plain_size) {
+    plain_str = env->GetStringUTFChars(result, NULL);
+    if (strlen(plain_str) >= (size_t)plain_size) {
         set_error("Plain buffer too small");
-        (*env)->ReleaseStringUTFChars(env, result, plain_str);
-        (*env)->DeleteLocalRef(env, result);
+        env->ReleaseStringUTFChars(result, plain_str);
+        env->DeleteLocalRef(result);
         return -1;
     }
     
     strcpy(plain_out, plain_str);
-    (*env)->ReleaseStringUTFChars(env, result, plain_str);
-    (*env)->DeleteLocalRef(env, result);
+    env->ReleaseStringUTFChars(result, plain_str);
+    env->DeleteLocalRef(result);
     
     return 0;
 }
@@ -367,14 +367,14 @@ void sm4_jni_cleanup(void) {
     if (g_jvm != NULL) {
         JNIEnv *env = sm4_jni_get_env();
         if (env != NULL && g_sm4_utils_class != NULL) {
-            (*env)->DeleteGlobalRef(env, g_sm4_utils_class);
+            env->DeleteGlobalRef(g_sm4_utils_class);
             g_sm4_utils_class = NULL;
         }
         if (env != NULL && g_sm4_keygen_class != NULL) {
-            (*env)->DeleteGlobalRef(env, g_sm4_keygen_class);
+            env->DeleteGlobalRef(g_sm4_keygen_class);
             g_sm4_keygen_class = NULL;
         }
-        (*g_jvm)->DestroyJavaVM(g_jvm);
+        g_jvm->DestroyJavaVM();
         g_jvm = NULL;
     }
 }
